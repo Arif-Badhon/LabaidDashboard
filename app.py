11c8dafd76296245e48c0898a0a4845076832bf5
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2022-01-30 01:02:26 +0600

    Added Figure

--- app.py
+++ app.py
@@ -51,7 +51,8 @@ app.layout = html.Div(children=[
             ),
                 html.Div(className='eight columns div-for-charts bg-grey',
                     children=[
-                        html.H2("Here you will get the visualization", style={'textAlign':'center'})
+                        html.H2("Here you will get the visualization", style={'textAlign':'center'}),
+                        dcc.Graph(id='company')
                     ]
                 )
         ]
@@ -59,6 +60,72 @@ app.layout = html.Div(children=[
 ]
 
 )
+
+@app.callback(
+    Output('company', 'figure'),
+    Input('ListDashboard', 'value'))
+def graph(ListDashboard):
+
+    fig={}
+
+    if ListDashboard == "Company Dashboard":
+        summary = collection[collection['Region'] == 'Summary']
+
+        total = summary[summary["Product"] == "Total"]
+        month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        quantity = total[["Jan_Qnt", "Feb_Qnt", "Mar_Qnt", "Apr_Qnt", "May_Qnt", "Jun_Qnt", "Jul_Qnt", "Aug_Qnt", "Sep_Qnt", "Oct_Qnt", "Nov_Qnt", "Dec_Qnt"]]
+        quantity = quantity.to_numpy()
+        quantity = quantity.tolist()
+        quantity = quantity[0]
+        sale = total[["Jan_Sales", "Feb_Sales", "Mar_Sales", "Apr_Sales", "May_Sales", "Jun_Sales", "Jul_Sales", "Aug_Sales", "Sep_Sales", "Oct_Sales", "Nov_Sales", "Dec_Sales"]]
+        sale = sale.to_numpy()
+        sale = sale.tolist()
+        sale = sale[0]
+
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=month, y=quantity, mode='lines', name='Quantity'))
+        fig.add_trace(go.Scatter(x=month, y=sale, mode='lines', name='Sales', yaxis="y2"))
+
+###########################
+        fig.update_layout(
+    # split the x-axis to fraction of plots in
+    # proportions
+    #xaxis=dict(
+        #domain=[0.3, 0.7]
+    #),
+   
+    # pass the y-axis title, titlefont, color
+    # and tickfont as a dictionary and store
+    # it an variable yaxis
+        yaxis=dict(
+            title="yaxis 1",
+            titlefont=dict(
+                color="#0000ff"
+            ),
+            tickfont=dict(
+                color="#0000ff"
+            )
+        ),
+        
+    # pass the y-axis 2 title, titlefont, color and
+    # tickfont as a dictionary and store it an
+    # variable yaxis 2
+        yaxis2=dict(
+            title="yaxis 2",
+            titlefont=dict(
+                color="#FF0000"
+            ),
+            tickfont=dict(
+                color="#FF0000"
+            ),
+            anchor="free",  # specifying x - axis has to be the fixed
+            overlaying="y",  # specifyinfg y - axis has to be separated
+            side="right",  # specifying the side the axis should be present
+            position=1  # specifying the position of the axis
+        ),
+    )
+    return fig
+
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
